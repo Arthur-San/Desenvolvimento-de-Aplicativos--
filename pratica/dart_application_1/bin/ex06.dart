84e17d0e36a168b37fbc0b74e9113477fdab4e4b
Arthur dos Santos <67018260+Arthur-San@users.noreply.github.com>
2021-07-04 12:13:36 -0300

    Create ex06.dart

---
+++ pratica/dart_application_1/bin/ex06.dart
@@ -0,0 +1,26 @@
+// Desenvolver um programa em Dart que calcule a média de 4 notas de um aluno.
+//Verifique se a média for maior ou igual a 7 o
+// aluno está aprovado e caso contrário Reprovado. Informe ainda o conceito de
+//acordo com a tabela: A - 10 - 9, B - 8.9 - 8, C -
+// 7.9 a 7, F - menor que 7
+
+void main() {
+  // ignore: unused_local_variable
+  var notas = [3, 2, 7, 9];
+
+  var soma = notas.elementAt(0) +
+      notas.elementAt(1) +
+      notas.elementAt(2) +
+      notas.elementAt(3);
+  var media = soma / notas.length;
+  print('Média do aluno $media');
+
+  if (media >= 7) {
+    var resultado = 'Aluno APROVADO\n';
+    print(resultado);
+  } else {
+    var resultado = 'Aluno REPROVADO\n';
+    var motivReprovacao = 'Motivo da Reprovação, notas: $notas';
+    print(resultado + motivReprovacao);
+  }
+}
